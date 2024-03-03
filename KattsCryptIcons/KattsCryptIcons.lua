45b9714add9a12e3be8f97d054d449f0da4d6bc5
DonjaZero <112839758+DonjaZero@users.noreply.github.com>
2024-03-02 21:51:52 -0500

    Initial release

---
+++ KattsCryptIcons/KattsCryptIcons.lua
@@ -0,0 +1,32 @@
+local ADDON_NAME  = "KattsCryptIcons"
+local MY_TEXTURES = {
+    "KattsCryptIcons/icons/KCAngry.dds",
+    "KattsCryptIcons/icons/KCBanHammer.dds",
+    "KattsCryptIcons/icons/KCBladeOfWoe.dds",
+    "KattsCryptIcons/icons/KCCHICKEN.dds",
+    "KattsCryptIcons/icons/KCCozy.dds",
+    "KattsCryptIcons/icons/KCDank.dds",
+    "KattsCryptIcons/icons/KCFart.dds",
+    "KattsCryptIcons/icons/KCHeart.dds",
+    "KattsCryptIcons/icons/KCHearts.dds",
+    "KattsCryptIcons/icons/KCHype.dds",
+    "KattsCryptIcons/icons/KCJeffrey.dds",
+    "KattsCryptIcons/icons/KCKattAvatar.dds",
+    "KattsCryptIcons/icons/KCKitty.dds",
+    "KattsCryptIcons/icons/KCKris.dds",
+    "KattsCryptIcons/icons/KCLOVE.dds",
+    "KattsCryptIcons/icons/KCPinkHeart.dds",
+    "KattsCryptIcons/icons/KCSwan.dds",
+    "KattsCryptIcons/icons/KCTank.dds",
+    "KattsCryptIcons/icons/KCXim.dds",
+}
+
+EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_ADD_ON_LOADED, function(_, addonName)
+    if addonName ~= ADDON_NAME then return end
+    EVENT_MANAGER:UnregisterForEvent(ADDON_NAME, EVENT_ADD_ON_LOADED)
+    -- check if OdySupportIcons is active and supports unique icon packs
+    if OSI and OSI.AddCustomIconPack then
+        -- add your list of icons
+        OSI.AddCustomIconPack(MY_TEXTURES)
+    end
+end)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Katt's Crypt Icons
+
+This is a custom OdySupportIcons pack made especially for Katt's Crypt, @Kattatonia's guilds on PCNA and PCEU. But everyone is welcome to use it!
+
+It requires the OdySupportIcons addon.  You will see the custom Katt's Crypt icons as choices when you assign custom icons to players via OdySupportIcons.
